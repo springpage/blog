022b17409f617a369e360ec4bfa13a1b7bbb213f
Spring Page <spring616666@gmail.com>
2018-06-25 21:03:34 -0700

    1

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,78 +1,75 @@
-import React,{Component} from 'react';
-import {Field, reduxForm} from 'redux-form';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {createPost} from '../actions';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
-class PostsNew extends Component{
+class PostsNew extends Component {
+  renderField(field) {
+    const {
+      meta: { touched, error }
+    } = field;
 
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
 
-  renderField(field){
-
-      const {meta:{touched, error}}=field;
-
-      const className=`form-group ${touched && error ? 'has-danger' : ''}`;
-
-      return(
-        <div className={className}>
-          <label>{field.label}</label>
-          <input
-            className='form-control'
-            type='text'
-            {...field.input}
-          />
-          <div className='text-help'>
-            {touched ? error : ""}
-          </div>
-        </div>
-      );
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">{touched ? error : ''}</div>
+      </div>
+    );
   }
 
-  onSubmit(values){
+  onSubmit(values) {
     this.props.createPost(values, () => {
       this.props.history.push('/');
     });
   }
 
-  render(){
-    const {handleSubmit}=this.props;
+  render() {
+    const { handleSubmit } = this.props;
 
-    return(
+    return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
-          label='Title For Post'
-          name='title'
+          label="Title For Post"
+          name="title"
           component={this.renderField}
         />
         <Field
-          label='Categories'
-          name='categories'
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
-          label='Post Content'
-          name='content'
+          label="Post Content"
+          name="content"
           component={this.renderField}
         />
-        <button type='submit' className='btn btn-primary'>Submit</button>
-        <Link to='/' className='btn btn-danger'>Cancel</Link>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+        <Link to="/" className="btn btn-danger">
+          Cancel
+        </Link>
       </form>
     );
   }
 }
 
-function validate(value){
-  const errors={};
+function validate(value) {
+  const errors = {};
 
-  if(!value.title){
-    errors.title='Enter a title!';
+  if (!value.title) {
+    errors.title = 'Enter a title!';
   }
 
-  if(!value.categories){
-    errors.categories='Enter some categories!';
+  if (!value.categories) {
+    errors.categories = 'Enter some categories!';
   }
-  if(!value.content){
-    errors.content='Enter some content please!';
+  if (!value.content) {
+    errors.content = 'Enter some content please!';
   }
 
   return errors;
@@ -82,5 +79,8 @@ export default reduxForm({
   validate,
   form: 'PostsNewForm'
 })(
-  connect(null,{createPost})(PostsNew)
+  connect(
+    null,
+    { createPost }
+  )(PostsNew)
 );
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,36 +1,35 @@
-import React,{Component} from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
-import {fetchPost, deletePost} from '../actions';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
-  componentDidMount(){
-      const {id} = this.props.match.params;
-      this.props.fetchPost(id);
-
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
   }
 
-  onDeleteClick(){
-    const {id} = this.props.match.params;
-    this.props.deletePost(id,()=>{
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
       this.props.history.push('/');
     });
   }
 
-  render(){
-    const {post}=this.props;
+  render() {
+    const { post } = this.props;
 
-    if(!post){
+    if (!post) {
       return <div>Loading...</div>;
     }
 
-
-    return(
+    return (
       <div>
-        <Link to='/' className='btn btn-primary'>Back to Index</Link>
+        <Link to="/" className="btn btn-primary">
+          Back to Index
+        </Link>
         <button
-          className='btn btn-danger pull-xs-right'
+          className="btn btn-danger pull-xs-right"
           onClick={this.onDeleteClick.bind(this)}
         >
           Delete Post
@@ -43,7 +42,10 @@ class PostsShow extends Component {
   }
 }
 
-function mapStateToProps({posts},ownProps){
-  return{post:posts[ownProps.match.params.id]}
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
 }
-export default connect(mapStateToProps,{fetchPost, deletePost})(PostsShow);
+export default connect(
+  mapStateToProps,
+  { fetchPost, deletePost }
+)(PostsShow);
